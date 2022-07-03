483729407ccd54754d6f8912afdb3da47046f9f6
ivan555-lab <vnchapay@gmail.com>
2022-07-03 09:38:24 +0300

    r3

--- tapp1/models.py
+++ tapp1/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 #fix1
+#testing change
 class PublishManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status='published')
